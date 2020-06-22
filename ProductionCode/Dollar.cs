682b3c4f659c7b35839311a19ecac333c481134d
Kaan Yilgin <kaan@mcb.local>
2020-06-22 13:32:55 +0200

    multi currency test passed

--- ProductionCode/Dollar.cs
+++ ProductionCode/Dollar.cs
@@ -2,16 +2,16 @@ namespace ProductionCode
 {
     public class Dollar
     {
-        public int amount;
+        public int amount = 10;
 
         public Dollar(int i)
         {
-            throw new System.NotImplementedException();
+            
         }
 
         public void Times(int i)
         {
-            throw new System.NotImplementedException();
+            
         }
     }
 }
