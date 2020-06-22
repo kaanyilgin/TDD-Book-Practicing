3c2539f0e317653752cd56a8cccd1a33db0e31c7
Kaan Yilgin <kaan@mcb.local>
2020-06-22 17:14:03 +0200

    degenerate object failed test

--- Test/MultiCurrencyMoneyTests.cs
+++ Test/MultiCurrencyMoneyTests.cs
@@ -14,5 +14,14 @@ namespace Test
             five.Times(2);
             Assert.That(10, Is.EqualTo(five.amount));
         }
+        
+        [Test]
+        public void TestMultiplicationTwoTimes()
+        {
+            Dollar five = new Dollar(5);
+            five.Times(2);
+            five.Times(3);
+            Assert.That(15, Is.EqualTo(five.amount));
+        }
     }
 }
