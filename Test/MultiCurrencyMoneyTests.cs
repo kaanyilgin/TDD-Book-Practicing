550319c60273433fddecf7b41d165dcaea38d61d
Kaan Yilgin <kaan@mcb.local>
2020-06-22 17:25:03 +0200

    degenerate object uncompile

--- Test/MultiCurrencyMoneyTests.cs
+++ Test/MultiCurrencyMoneyTests.cs
@@ -12,16 +12,17 @@ namespace Test
         {
             Dollar five = new Dollar(5);
             five.Times(2);
-            Assert.That(10, Is.EqualTo(five.amount));
+            Assert.That(five.amount, Is.EqualTo(10));
         }
-        
+
         [Test]
         public void TestMultiplicationTwoTimes()
         {
             Dollar five = new Dollar(5);
-            five.Times(2);
-            five.Times(3);
-            Assert.That(15, Is.EqualTo(five.amount));
+            Dollar product = five.Times(2);
+            Assert.That(product.amount, Is.EqualTo(10));
+            product.Times(3);
+            Assert.That(five.amount, Is.EqualTo(15));
         }
     }
 }
