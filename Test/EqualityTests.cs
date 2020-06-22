5b5030b0a99b63276d8c3dcc0efc6ddc27ded0b0
Kaan Yilgin <kaan@mcb.local>
2020-06-23 00:06:25 +0200

    franc equal tests added

--- Test/EqualityTests.cs
+++ Test/EqualityTests.cs
@@ -10,12 +10,9 @@ namespace Test
         public void testEqualtity5to5()
         {
             Assert.That(new Dollar(5).Equals(new Dollar(5)), Is.True);
-        }
-        
-        [Test]
-        public void testEqualtity5to6()
-        {
             Assert.That(new Dollar(5).Equals(new Dollar(6)), Is.False);
+            Assert.That(new Franc(5).Equals(new Franc(5)), Is.True);
+            Assert.That(new Franc(5).Equals(new Franc(6)), Is.False);
         }
     }
 }
